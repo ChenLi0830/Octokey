de931c40e260c271d4cf2c746c219d696f24c1ee
li chen <chen.li@nocland.com>
2016-01-31 11:43:10 -0330

    Layout UI fix for AppLoading component

--- client/components/AppLoading.jsx
+++ client/components/AppLoading.jsx
@@ -11,7 +11,7 @@ const {RefreshIndicator,Paper  } = MUI;
 
 AppLoading = React.createClass({
     render() {
-        return <div id="layout-margin">
+        return <div>
             <Paper zDepth={1}
                    style={{backgroundColor:"#ffffff", boxShadow:"0 1px 6px rgba(0, 0, 0, 0.12)", padding:0, borderRadius:"5px"}}>
                 <div className="horizontal-center" style={{width:"40px", padding:"40px"}}>
