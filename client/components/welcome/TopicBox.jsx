61992a5dd54f2ec0d42f30e0d09e103dee777457
Chen Li <yichen.li0830@gmail.com>
2016-05-12 22:50:46 -0230

    UX improvement: Topic choose - images 一个一个load进去，体验不好 #22

--- client/components/welcome/TopicBox.jsx
+++ client/components/welcome/TopicBox.jsx
@@ -8,15 +8,16 @@
  *******************************************************************************/
 
 const styles = {
-  icon: {height: "100%", width: "100%", borderRadius: "10px"},
+  icon: {height: "100%", width: "100%", borderRadius: "10px", transitionDuration:"0.5s"},
   topicNameBox: {bottom: 33, left: 38, color: Colors.white, position: "absolute"},
   topicName: {fontWeight: 200, fontSize: 16, /*textShadow: "0px 0px 30px #333"*/},
-  imgBox: {margin: 10},
+  imgBox: {/*margin: 10*/margin: 0},
   circleCheck: {height: 25, top: "2.5em", right: "2.9em", position: "absolute"},
 };
 
 const {Col, Row, Grid} = ReactBootstrap;
 
+let boxSize;
 const TopicBox = React.createClass({
   propTypes: {
     topicId: React.PropTypes.string.isRequired,
@@ -31,20 +32,30 @@ const TopicBox = React.createClass({
   getInitialState(){
     return {
       focused: false,
+      height: "0px",
+      loaded: false,
     }
+  },
+
+  componentDidMount(){
+    boxSize = ReactDOM.findDOMNode(this.refs.boxCol).offsetWidth;
+    this.setState({height:boxSize+'px'});
   },
 
   render(){
     const {topicId, topicName, topicRank} = this.props;
     return <Col xs={3} style={{padding:15}}>
       {<div style={{cursor:"pointer"}}
+            ref="boxCol"
             onClick={this.props.whenTopicClicked.bind(null, topicId, topicName, topicRank)}
             onMouseOver={()=>{this.setState({focused:true})}}
             onMouseOut={()=>{this.setState({focused:false})}}>
 
-        <div style={styles.imgBox}>
-          <img src={this.props.iconURL}
-               style={_.extend({},styles.icon,{opacity:this.state.focused?0.85:1})}/>
+        <div style={_.extend({},styles.imgBox,{height:this.state.height, width:"100%", backgroundColor: "gainsboro", borderRadius:"10px"})}>
+          <img src={/*"http://deelay.me/1000/localhost:3000/"+*/this.props.iconURL}
+               style={_.extend({},styles.icon,{opacity:this.state.loaded?(this.state.focused? 0.85:1):0})}
+               onLoad = {()=>{this.setState({loaded:true})}}
+          />
         </div>
 
         <div style={styles.topicNameBox}>
