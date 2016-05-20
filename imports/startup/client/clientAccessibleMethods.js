73075ac3924b5093392db8658b41e32549b08fac
Chen Li <yichen.li0830@gmail.com>
2016-05-20 16:32:30 -0230

    Make sure the stub method works for latency compensation part 1 (todo - make ZenApp available for client side)

---
+++ imports/startup/client/clientAccessibleMethods.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Chen on 2016-05-20.
+ */
+import '../../api/generalMethods.js';
+import '../../api/topics.js';
+import '../../api/userAppCredentials.js';
+import '../../api/userApps.js';
+import '../../api/users.js';
+import '../../api/zenApps.js';
+import '../../api/zenCategories.js';
