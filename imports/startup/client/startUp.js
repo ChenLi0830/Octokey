e95691bf46c31ee5e55b374a0a786a080faaee88
Chen Li <yichen.li0830@gmail.com>
2016-05-26 12:37:51 -0230

    Console.log 在oyaoshi网页console中很多，影响使用体验。 #40 - Hide all the console logs in production mode

--- imports/startup/client/startUp.js
+++ imports/startup/client/startUp.js
@@ -1,1 +1,10 @@
 T9n.setLanguage("zh_cn");
+
+if (Meteor.isProduction){
+  console = console || {};
+  console.log = function(){};
+}
+
+if (Meteor.isDevelopment){
+  console.log("Under developer mode")
+}
